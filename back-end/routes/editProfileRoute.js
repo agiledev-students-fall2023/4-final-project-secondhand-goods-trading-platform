e0b98f63c6cef2ea69418a3550b3860dd3a53e06
anitayyc <yy3557@nyu.edu>
2023-11-13 19:49:49 -0500

    add accountRoute and editPorfileRoute tests, fix minor issue in editProfileRoute

--- back-end/routes/editProfileRoute.js
+++ back-end/routes/editProfileRoute.js
@@ -6,6 +6,9 @@ const users = require('../data/users');
 router.put('/edit-profile', (req, res) => {
   const username = req.query.username; // or get from req.body
   const userIndex = users.findIndex((u) => u.username === username);
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).json({ message: 'Invalid update data' });
+  }
 
   if (userIndex !== -1) {
     users[userIndex] = { ...users[userIndex], ...req.body };
