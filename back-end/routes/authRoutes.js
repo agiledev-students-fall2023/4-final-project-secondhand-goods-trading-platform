e9fd24f9587681a5a3ca0c153b723394fbf72ad2
SiqiiW <sw4895@nyu.edu>
2023-11-27 01:58:12 -0500

    set up shared mongodb, express-validator'ed signup and login process

--- back-end/routes/authRoutes.js
+++ back-end/routes/authRoutes.js
@@ -2,15 +2,20 @@ const express = require('express');
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-
+const { body, validationResult } = require('express-validator');
 
 const router = express.Router();
 const User = require("../models/User.js");
 
 
 
+
 // Login endpoint
-router.post('/login', async (req, res) => {
+router.post('/login', [
+  body('username').not().isEmpty().trim().escape(),
+  body('email').isEmail().normalizeEmail(),
+  body('password').not().isEmpty(),
+], async (req, res) => {
   console.log("we are in login!");
   const { username, email, password } = req.body;
 
@@ -36,9 +41,25 @@ router.post('/login', async (req, res) => {
 });
 
 
-router.post('/signup', async (req, res) => {
+
+router.post('/signup', [
+  body('username')
+    .not().isEmpty().withMessage('Username cannot be empty')
+    .trim().escape()
+    .isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
+  body('email')
+    .isEmail().withMessage('Email must be a valid email address')
+    .normalizeEmail(),
+  body('password')
+    .isStrongPassword().withMessage('Password must be strong (at least 8 characters, including 1 uppercase, 1 lowercase, 1 number, and 1 symbol)'),
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const errorMessages = errors.array().map(err => ({ msg: err.msg, param: err.param }));
+    return res.status(400).json({ message: 'Validation errors', errors: errorMessages });
+  }
   const { username, email, password } = req.body;
-  console.log('Signup route hit');
+  console.log('we are in signup!');
 
   // Check if the user already exists
   const userExists = await User.findOne({ $or: [{ username }, { email }] });
@@ -46,8 +67,6 @@ router.post('/signup', async (req, res) => {
   if (userExists) {
     return res.status(400).json({ message: 'User already exists' });
   }
-
-  // Hash the password
 
   // Create a new user instance and save to the database
   const newUser = new User({ username, email, password});
